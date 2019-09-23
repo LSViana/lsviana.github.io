40cfc8706414a721e0a81808c758e9d201bbeefc
LSViana <lv201122@gmail.com>
2019-09-23 00:02:35 -0300

    Added project Eventually information and base for next projects

--- src/components/ProjectDialog.vue
+++ src/components/ProjectDialog.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="project-dialog-root pa-6">
     <div class="d-flex flex-column align-stretch">
-      <h3 class="bold font-weight-light light-gray--text">
-        Project's Information
-      </h3>
+      <h2 class="bold font-weight-light dark-gray--text">
+        Project Information
+      </h2>
     </div>
-    <div class="my-3 project-info-container">
+    <div class="my-5 project-info-container">
       <component v-if="projectName" :is="'project-' + projectName" />
     </div>
     <div class="d-flex flex-row justify-end">
-      <a href="" class="close-button text-uppercase bold font-weight-light white--text primary py-3 px-5 pointer" @click.prevent="$emit('close')">
+      <a href="" class="close-button text-uppercase bold font-weight-light white--text primary py-3 px-5 pointer size-6" @click.prevent="$emit('close')">
         Close
       </a>
-    </div>
+    </div>    
   </div>
 </template>
 
@@ -31,13 +31,12 @@ export default {
 @import '../styles/Variables.scss';
 
 .project-dialog-root {
+  border: 4px $primaryColor dashed;
+  border-radius: nth($sizes, 4);
   max-width: 90vw;
-  width: 720px;
+  width: 900px;
   background-color: white;
 
-  .close-button {
-    font-size: 1.25em;
-  }
   .close-button:hover {
     color: $accentColor;
   }
--- src/components/projects/ProjectEventually.vue
+++ src/components/projects/ProjectEventually.vue
@@ -1,281 +1,55 @@
 <template>
-  <div>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
-    <p>
-      Projetão Eventually
-    </p>
+  <div class="project-info-body d-flex flex-column align-stretch">
+    <div class="d-flex flex-column align-stretch">
+      <h3 class="font-weight-light light-gray--text">
+        Title and Date
+      </h3>
+      <div class="size-6 project-title d-flex flex-row align-center flex-wrap">
+        <h2 class="size-7 bold font-weight-light py-3 px-5 primary accent--text border-radius-3">
+          Eventually
+        </h2>
+        <p>
+          developed from <span>Feb, 2019</span> to <span>Dec, 2019</span>.
+        </p>
+      </div>
+    </div>
+    <div class="d-flex flex-column align-stretch mt-5">
+      <h3 class="font-weight-light light-gray--text">
+        Place
+      </h3>
+      <div class="size-6 d-flex flex-row align-center flex-wrap">
+        <p class="bold font-weight-light px-3 primary--text">
+          Instituto Federal de São Paulo
+        </p>
+        <p>
+          while studying <span>Software Development Technical</span>.
+        </p>
+      </div>
+    </div>
+    <div class="d-flex flex-column align-stretch mt-5">
+      <h3 class="font-weight-light light-gray--text">
+        Problem solved
+      </h3>
+      <div class="size-6 d-flex flex-row align-center flex-wrap">
+        <p>
+          Event management with <span>custom PDF certificates</span>, which are signed and may be validated, attendance and vacancies control and lots of customization to make the task easier.
+        </p>
+      </div>
+    </div>
+    <div class="d-flex flex-column align-stretch mt-5">
+      <h3 class="font-weight-light light-gray--text">
+        Demo
+      </h3>
+      <div class="project-images size-6 d-flex flex-row flex-wrap">
+        <div>
+          <a href="https://i.imgur.com/5paD99V.png" target="blank">
+            <div>
+              <img src="https://i.imgur.com/5paD99V.png" alt="Eventually's Home Page">
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -285,6 +59,27 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../../styles/Variables.scss';
 
+.project-info-body {
+  & > div > div > p > span {
+    @extend .px-3;
+    font-family: $boldFont;
+    color: $primaryColor;
+  }
+  & > div > div > p > span:last-child {
+    padding-right: 0px;
+  }
+  .project-images {
+    & > div {
+      @extend .mx-2, .my-1;
+      width: 49%;
+      & img {
+        width: 100%;
+        border: nth($sizes, 3) $accentColor dashed;
+      }
+    }
+  }
+}
 </style>
