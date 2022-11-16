06366908a57fa076c28cce0299310bf3d9733790
Lucas Viana <lv201122@gmail.com>
2022-11-15 22:50:03 -0300

    Add sample styles to the home page using TailwindCSS

--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Lucas Viana</h1>
-    <p>I'm a web software developer.</p>
+    <div class="m-6 p-3 border rounded-lg bg-slate-100">
+      <h1 class="text-3xl">Lucas Viana</h1>
+      <p>I'm a web software developer.</p>
+    </div>
   </div>
 </template>
