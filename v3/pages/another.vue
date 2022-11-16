cc79c3ca6fc0cc396a61ff2fcd4ddb4beda7af42
Lucas Viana <lv201122@gmail.com>
2022-11-16 08:18:14 -0300

    Add another page to test TailwindCSS

---
+++ v3/pages/another.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="m-6 p-3 rounded-lg bg-amber-200 border border-amber-400">
+      Another page.
+    </div>
+  </div>
+</template>
--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="m-6 p-3 border rounded-lg bg-slate-100">
+    <div class="m-6 p-3 rounded-lg bg-red-200 border border-red-400">
       <h1 class="text-3xl">Lucas Viana</h1>
       <p>I'm a web software developer.</p>
     </div>
