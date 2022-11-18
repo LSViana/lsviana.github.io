0caf521f3fb61ee91bb4293794f03bbcfaa5678b
Lucas Viana <lv201122@gmail.com>
2022-11-18 00:10:28 -0300

    Format existing files

--- v3/nuxt.config.ts
+++ v3/nuxt.config.ts
@@ -1,6 +1,7 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss'
-    ]
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="m-6 p-3 rounded-lg bg-red-200 border border-red-400">
-      <h1 class="text-3xl">Lucas Viana</h1>
+      <h1 class="text-3xl">
+        Lucas Viana
+      </h1>
       <p>I'm a web software developer.</p>
     </div>
   </div>
