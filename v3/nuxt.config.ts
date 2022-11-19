9f84bd3cb6ddf7fe818fb45510fa15dbebc57885
Lucas Viana <lv201122@gmail.com>
2022-11-19 00:10:05 -0300

    Add "Sora" family from Google Fonts

--- v3/nuxt.config.ts
+++ v3/nuxt.config.ts
@@ -3,5 +3,30 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app: {
+    head: {
+      title: 'Lucas Viana\'s Résumé',
+      /**
+       * <link rel="preconnect" href="https://fonts.googleapis.com">
+       * <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+       * <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+       */
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Sora&display=swap'
+        }
+      ]
+    }
+  }
 })
