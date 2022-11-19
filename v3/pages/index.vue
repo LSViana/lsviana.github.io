4a957735ee74c21fedfe8598a8a03608f37e17a8
Lucas Viana <lv201122@gmail.com>
2022-11-19 00:10:13 -0300

    Update the home page's layout

--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,15 +1,10 @@
-<script setup lang="ts">
-import HomeSample from '~/components/home/HomeSample.vue'
-</script>
-
 <template>
-  <div>
-    <div class="m-6 p-3 rounded-lg bg-red-200 border border-red-400">
-      <h1 class="text-3xl">
-        Lucas Viana
+  <div class="bg-slate-100 text-slate-700 dark:bg-slate-800 dark:text-slate-200 p-5 min-h-screen flex items-center justify-center">
+    <div class="text-center p-3 md:p-8 flex flex-col space-y-3 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+      <h1 class="text-5xl md:text-8xl tracking-tight">
+        LSViana
       </h1>
-      <p>I'm a web software developer.</p>
+      <p>Web Software Developer</p>
     </div>
-    <HomeSample />
   </div>
 </template>
