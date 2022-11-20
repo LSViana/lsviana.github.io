05f6eafb6b8dacbadfdd2d7df93ea7fe792625f2
Lucas Viana <lv201122@gmail.com>
2022-11-20 13:46:00 -0300

    Increment website content to work as a temporary solution

--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,10 +1,86 @@
 <template>
   <div class="bg-slate-100 text-slate-700 dark:bg-slate-800 dark:text-slate-200 p-5 min-h-screen flex items-center justify-center">
-    <div class="text-center p-3 md:p-8 flex flex-col space-y-3 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
-      <h1 class="text-5xl md:text-8xl tracking-tight">
-        LSViana
-      </h1>
-      <p>Web Software Developer</p>
+    <div class="flex flex-col space-y-4">
+      <div class="text-center p-3 md:p-8 flex flex-col space-y-3 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+        <h1 class="text-5xl md:text-8xl tracking-tight">
+          LSViana
+        </h1>
+        <p>Web Software Developer</p>
+      </div>
+      <div class="text-center p-3 md:p-8 flex flex-col space-y-5 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+        <div>
+          <div class="uppercase opacity-50 text-sm mb-0">
+            Front-end
+          </div>
+          <div>
+            <span>Vue.js</span>
+            <span class="select-none"> · </span>
+            <span>Nuxt.js</span>
+            <span class="select-none"> · </span>
+            <span>TypeScript</span>
+            <span class="select-none"> · </span>
+            <span>Playwright</span>
+            <span class="select-none"> · </span>
+            <span>Storybook</span>
+          </div>
+        </div>
+        <div>
+          <div class="uppercase opacity-50 text-sm mb-0">
+            Back-end
+          </div>
+          <div>
+            <span>C#</span>
+            <span class="select-none"> · </span>
+            <span>ASP.NET Core</span>
+            <span class="select-none"> · </span>
+            <span>PostgreSQL</span>
+            <span class="select-none"> · </span>
+            <span>Redis</span>
+            <span class="select-none"> · </span>
+            <span>Azure</span>
+          </div>
+        </div>
+        <div>
+          <div class="uppercase opacity-50 text-sm mb-0">
+            Design & QA
+          </div>
+          <div>
+            <span>Figma</span>
+            <span class="select-none"> · </span>
+            <span>Storybook</span>
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3 md:p-8 flex flex-col space-y-5 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+        <div>
+          <div class="uppercase opacity-50 text-sm mb-0">
+            Contact
+          </div>
+          <div>
+            <a href="https://github.com/LSViana" target="_blank" rel="noopener">GitHub</a>
+            <span class="select-none"> · </span>
+            <a href="https://linkedin.com/in/lsviana" target="_blank" rel="noopener">LinkedIn</a>
+            <span class="select-none"> · </span>
+            <a href="mailto:lv201122@gmail.com" target="_blank" rel="noopener">Email</a>
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3 md:p-8 flex flex-col space-y-5 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+        <div>
+          <div class="uppercase opacity-50 text-sm mb-0">
+            Made with
+          </div>
+          <div>
+            <span>Nuxt.js</span>
+            <span class="select-none"> · </span>
+            <span>Vite</span>
+            <span class="select-none"> · </span>
+            <span>TailwindCSS</span>
+            <span class="select-none"> · </span>
+            <span>❤️</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
