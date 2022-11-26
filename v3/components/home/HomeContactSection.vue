686b19c3c29b10c9f893fcfcff29757c447e2049
Lucas Viana <lv201122@gmail.com>
2022-11-26 18:48:55 -0300

    Add HomeContactSection

---
+++ v3/components/home/HomeContactSection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const contacts = [
+  { text: 'GitHub', href: 'https://github.com/LSViana' },
+  { text: 'LinkedIn', href: 'https://linkedin.com/in/lsviana' },
+  { text: 'Email', href: 'mailto:lv201122@gmail.com' }
+]
+</script>
+
+<template>
+  <HomeSectionCard>
+    <HomeSectionList title="Contact" :items="contacts">
+      <template #item="{ item }">
+        <a :href="item.href" target="_blank" class="underline underline-offset-4 decoration-wavy">{{ item.text }}</a>
+      </template>
+    </HomeSectionList>
+  </HomeSectionCard>
+</template>
