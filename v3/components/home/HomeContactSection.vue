5f46081fe88e9bc2a06ff2121883c40f536aa065
Lucas Viana <lv201122@gmail.com>
2022-12-03 15:17:11 -0300

    Adapt link colors for light and dark modes

--- v3/components/home/HomeContactSection.vue
+++ v3/components/home/HomeContactSection.vue
@@ -10,7 +10,7 @@ const contacts = [
   <HomeSectionCard>
     <HomeSectionList title="Contact" :items="contacts">
       <template #item="{ item }">
-        <a :href="item.href" target="_blank" class="underline underline-offset-4 decoration-wavy text-blue-400 hover:text-blue-300">
+        <a :href="item.href" target="_blank" class="underline underline-offset-4 decoration-wavy text-blue-700 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">
           {{ item.text }}
         </a>
       </template>
