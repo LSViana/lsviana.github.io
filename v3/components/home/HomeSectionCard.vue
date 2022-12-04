e1428bac6f2076a6f077aad477e7ce26ad6c27f7
Lucas Viana <lv201122@gmail.com>
2022-12-04 17:22:59 -0300

    Add dense property to the HomeSectionCard component

--- v3/components/home/HomeSectionCard.vue
+++ v3/components/home/HomeSectionCard.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  dense: boolean
+}>(), {
+  dense: false
+})
+
+</script>
+
 <template>
-  <div class="text-center p-3 md:p-8 flex flex-col space-y-5 bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600">
+  <div
+    class="text-center p-3 md:p-8 flex flex-col bg-slate-300 dark:bg-slate-700 rounded-lg border border-slate-600"
+    :class="[props.dense ? 'space-y-3 md:space-y-5' : 'space-y-5']"
+  >
     <slot />
   </div>
 </template>
