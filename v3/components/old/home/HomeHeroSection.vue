20264753e3188e3e6658f1d89df6c76321c129ed
Lucas Viana <lv201122@gmail.com>
2023-03-25 12:17:15 -0300

    Drop old components of the home page

--- v3/components/old/home/HomeHeroSection.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <OldHomeSectionCard dense>
-    <h1 class="text-5xl md:text-8xl tracking-tight">
-      LSViana
-    </h1>
-    <p>Web Software Developer</p>
-  </OldHomeSectionCard>
-</template>
