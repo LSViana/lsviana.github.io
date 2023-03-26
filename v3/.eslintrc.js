23f56342ad7c81ee67d39b57b7ec7fd851324471
Lucas Viana <lv201122@gmail.com>
2023-03-26 19:55:12 -0300

    Update ESLint rules not to wrap inline components (this could cause layout issues)

--- v3/.eslintrc.js
+++ v3/.eslintrc.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   rules: {
     'vue/no-multiple-template-root': 'off',
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': ['error', {
+      ignores: [
+        'span',
+        'pre',
+        'code',
+        'b',
+        'i',
+        'strong',
+        'em',
+        'a',
+        'LvLink'
+      ]
+    }]
   }
 }
