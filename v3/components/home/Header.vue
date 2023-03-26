783d21a15deb929b15a827481919cf6daffa0481
Lucas Viana <lv201122@gmail.com>
2023-03-26 20:05:12 -0300

    Add the header component

---
+++ v3/components/home/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import LvLink from '~/components/shared/LvLink.vue'
+</script>
+
+<template>
+  <nav class="flex justify-between items-center border-b-slate-600 border-b-2 pb-6 mb-4">
+    <h3 class="text-xl">
+      <NuxtLink to="/">
+        @lsviana
+      </NuxtLink>
+    </h3>
+    <ul class="flex gap-4">
+      <li>
+        <LvLink to="/" active-class="font-bold">
+          Work
+        </LvLink>
+      </li>
+      <li>
+        <LvLink to="/skills" active-class="font-bold">
+          Skills
+        </LvLink>
+      </li>
+    </ul>
+  </nav>
+</template>
