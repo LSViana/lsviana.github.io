bbb7dcfeed995b04d73458ed4f96313e2117d361
Lucas Viana <lv201122@gmail.com>
2023-03-27 23:10:03 -0300

    Add the shared LvLink component

---
+++ v3/components/shared/LvLink.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  to?: string,
+  href?: string,
+  target?: '_blank' | '_self' | '_parent' | '_top',
+  activeClass?: string
+}>(), {
+  activeClass: '',
+  target: '_self'
+})
+</script>
+
+<template>
+  <NuxtLink
+    class="underline underline-offset-4 text-blue-700
+      hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300"
+    :to="props.to"
+    :href="props.href"
+    :target="props.target"
+    :active-class="props.activeClass"
+  >
+    <slot />
+  </NuxtLink>
+</template>
