8120b5688ab7c446ab37859af675264f3205deba
Lucas Viana <lv201122@gmail.com>
2023-03-27 23:19:23 -0300

    Update the home page

--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -1,10 +1,56 @@
+<script setup lang="ts">
+import LvLink from '~/components/shared/LvLink.vue'
+</script>
+
 <template>
-  <div class="bg-slate-100 text-slate-700 dark:bg-slate-800 dark:text-slate-200 p-5 min-h-screen flex items-center justify-center">
-    <div class="flex flex-col space-y-4">
-      <OldHomeHeroSection />
-      <OldHomeKnowledgeSection />
-      <OldHomeContactSection />
-      <OldHomeMadeWithSection />
-    </div>
-  </div>
+  <section>
+    <h2 class="text-3xl font-bold">
+      2023
+    </h2>
+    <ul class="mt-4 flex flex-col gap-4">
+      <li>
+        <p>Personal website redesign</p>
+        <small>
+          — Previously
+          <LvLink href="https://web.archive.org/web/20220326102751/https://lsviana.github.io/" target="_blank">2019-2022</LvLink>,
+          <LvLink href="https://web.archive.org/web/20230329023911/https://lsviana.github.io/" target="_blank">2023</LvLink>
+        </small>
+      </li>
+      <li>
+        <p>CRDT contribution to add support for C#</p>
+        <small>
+          — Worked on .NET bindings for
+          <LvLink href="https://github.com/y-crdt/y-crdt" target="_blank">Yrs</LvLink>
+        </small>
+      </li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      2020
+    </h2>
+    <ul class="mt-4 flex flex-col gap-4">
+      <li>
+        <p>TestDome</p>
+        <small>— Product designer</small>
+        <br>
+        <small>— Software developer</small>
+        <br>
+        <small>
+          — Redesigned from
+          <LvLink href="https://web.archive.org/web/20200415013110/https://www.testdome.com/" target="_blank">this</LvLink>
+          into
+          <LvLink href="https://web.archive.org/web/20230318002236/https://www.testdome.com/" target="_blank">this</LvLink>
+        </small>
+        <br>
+        <small>— Worked on updating the tech stack (Bootstrap 3, jQuery, ASP.NET Framework, etc.)</small>
+      </li>
+      <li>
+        <p>Digitalk</p>
+        <small>— Software developer</small>
+        <br>
+        <small>— Worked on React web apps</small>
+      </li>
+    </ul>
+  </section>
 </template>
