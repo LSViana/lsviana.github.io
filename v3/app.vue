8442c3825b2c4ba18d8e2b93e762adfb22d7c40f
Lucas Viana <lv201122@gmail.com>
2023-03-27 23:11:11 -0300

    Update the app layout to render the header and identification around all pages

--- v3/app.vue
+++ v3/app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import Header from '~/components/home/Header.vue'
+import Identification from '~/components/home/Identification.vue'
+</script>
+
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <div class="bg-slate-100 text-slate-700 dark:bg-slate-800 dark:text-slate-200 min-h-screen flex justify-center">
+      <div class="container flex flex-col space-y-4 py-8 px-4">
+        <Header />
+        <main class="grid md:grid-cols-6 md:gap-x-4 gap-y-4 flex-grow">
+          <Identification class="col-span-2" />
+          <div class="col-span-4">
+            <NuxtPage />
+          </div>
+        </main>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
