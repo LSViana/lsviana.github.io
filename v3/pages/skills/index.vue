3bb3eb644c91f40b7966b8b3e24faa6ca7a3dd3a
Lucas Viana <lv201122@gmail.com>
2023-03-27 23:35:01 -0300

    Introduce the skills page

---
+++ v3/pages/skills/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h2 class="text-3xl font-bold">
+      Front-end
+    </h2>
+    <ul class="mt-4 flex flex-col gap-2">
+      <li>Vue.js</li>
+      <li>Nuxt.js</li>
+      <li>TypeScript</li>
+      <li>Playwright</li>
+      <li>Storybook</li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      Back-end
+    </h2>
+    <ul class="mt-4 flex flex-col gap-2">
+      <li>C#</li>
+      <li>ASP.NET Core</li>
+      <li>PostgreSQL</li>
+      <li>Redis</li>
+      <li>Azure</li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      Design
+    </h2>
+    <ul class="mt-4 flex flex-col gap-2">
+      <li>Figma</li>
+      <li>Storybook</li>
+      <li>Jest</li>
+    </ul>
+  </section>
+</template>
