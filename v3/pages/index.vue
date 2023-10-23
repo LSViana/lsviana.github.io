0cee72331cb75ab23e6455ef41c43259fad5a3f3
LSViana <lv201122@gmail.com>
2023-10-22 23:05:24 -0300

    Add more content for the "Work & Awards" page

--- v3/pages/index.vue
+++ v3/pages/index.vue
@@ -4,7 +4,7 @@ import LvLink from '~/components/shared/LvLink.vue'
 
 <template>
   <section>
-    <h2 class="text-3xl font-bold">
+    <h2 class="mt-4 text-3xl font-bold">
       2023
     </h2>
     <ul class="mt-4 flex flex-col gap-4">
@@ -15,6 +15,8 @@ import LvLink from '~/components/shared/LvLink.vue'
           <LvLink href="https://web.archive.org/web/20220326102751/https://lsviana.github.io/" target="_blank">2019-2022</LvLink>,
           <LvLink href="https://web.archive.org/web/20230329023911/https://lsviana.github.io/" target="_blank">2023</LvLink>
         </small>
+        <br>
+        <small>— Worked on design & development (Figma & Vue)</small>
       </li>
       <li>
         <p>Y-CRDT contribution as C# developer</p>
@@ -35,6 +37,31 @@ import LvLink from '~/components/shared/LvLink.vue'
           <LvLink href="https://web-labs.vercel.app/" target="_blank">Web Labs</LvLink>
         </p>
         <small>— Experiments on the web platform</small>
+        <br>
+        <small>— Worked on experimenting with Web APIs</small>
+      </li>
+      <li>
+        <p>
+          National Gold Medalist
+        </p>
+        <small>
+          — Brazil national IT competition (<LvLink href="https://web.archive.org/web/20220326102810/https://youtu.be/G9noGCId7x0?t=1354" target="_blank">Olimpíadas do Conhecimento 2022</LvLink>)
+        </small>
+      </li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      2021
+    </h2>
+    <ul class="mt-4 flex flex-col gap-4">
+      <li>
+        <p>
+          Americas Gold Medalist
+        </p>
+        <small>
+          — Americas continental IT competition (<LvLink href="https://web.archive.org/web/20220326102810/https://youtu.be/pLlB-Nq6b8E?t=1329" target="_blank">WorldSkills Guatemala 2021</LvLink>)
+        </small>
       </li>
     </ul>
   </section>
@@ -63,6 +90,52 @@ import LvLink from '~/components/shared/LvLink.vue'
         <small>— Software developer</small>
         <br>
         <small>— Worked on React web apps</small>
+      </li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      2019
+    </h2>
+    <ul class="mt-4 flex flex-col gap-4">
+      <li>
+        <p>
+          State Gold Medalist
+        </p>
+        <small>
+          — São Paulo state IT competition (<LvLink href="https://web.archive.org/web/20220326102810/https://saopauloskills.sp.senai.br/" target="_blank">São Paulo Skills 2019</LvLink>)
+        </small>
+      </li>
+      <li>
+        <p>
+          Americas Finalist
+        </p>
+        <small>
+          — Competitor at <LvLink href="https://web.archive.org/web/20220326102810/https://imaginecup.microsoft.com/en-us/Team/1243c298-1463-460d-b7bd-3450bbb8248d" target="_blank">Microsoft Imagine Cup 2019</LvLink>
+        </small>
+      </li>
+    </ul>
+  </section>
+  <section class="mt-10">
+    <h2 class="text-3xl font-bold">
+      2017
+    </h2>
+    <ul class="mt-4 flex flex-col gap-4">
+      <li>
+        <p>
+          State Gold Medalist
+        </p>
+        <small>
+          — São Paulo state IT competition (<LvLink href="https://web.archive.org/web/20220326102810/https://saopauloskills.sp.senai.br/" target="_blank">São Paulo Skills 2017</LvLink>)
+        </small>
+      </li>
+      <li>
+        <p>
+          Outstanding High School Student
+        </p>
+        <small>— Student at national public school</small>
+        <br>
+        <small>— Watch the <LvLink href="https://www.youtube.com/watch?v=f5jvV2mVM08" target="_blank">advertisement video</LvLink></small>
       </li>
     </ul>
   </section>
